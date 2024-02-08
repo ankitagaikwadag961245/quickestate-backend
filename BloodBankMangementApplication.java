f1267feec68aa881bbb97f9a992691e1ed164463
ankitagaikwadag961245 <158686136+ankitagaikwadag961245@users.noreply.github.com>
2024-02-08 14:14:36 +0530

    Add files via upload
    add new file

---
+++ BloodBankMangementApplication.java
@@ -0,0 +1,13 @@
+package com.technosignia.com.BloodBankMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankMangementApplication.class, args);
+	}
+
+}
